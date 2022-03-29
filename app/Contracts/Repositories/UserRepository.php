d5b205353143b73dd71cc21a4050c4cfa419d0eb
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-28 20:48:08 -0500

    Added find user by email method

--- app/Contracts/Repositories/UserRepository.php
+++ app/Contracts/Repositories/UserRepository.php
@@ -2,7 +2,16 @@
 
 namespace App\Contracts\Repositories;
 
+use App\Models\User;
+
 interface UserRepository extends Repository
 {
-
+    /**
+     * Find one user by email
+     *
+     * @param  string $email
+     * @param  string $relationships
+     * @return \App\Models\User
+     */
+    public function findOneByEmail(string &$email, array $relationships = []);
 }
