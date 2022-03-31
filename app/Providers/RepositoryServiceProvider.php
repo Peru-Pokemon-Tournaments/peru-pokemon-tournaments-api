5997381f590422cfff6042c93f5a11f89faddc73
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-31 16:29:20 -0500

    Added repositories provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -17,8 +17,44 @@ class RepositoryServiceProvider extends ServiceProvider
             \App\Contracts\Repositories\CompetitorRepository::class,
             \App\Repositories\CompetitorRepository::class);
         $this->app->bind(
+            \App\Contracts\Repositories\DeviceRepository::class,
+            \App\Repositories\DeviceRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\ExternalBracketRepository::class,
+            \App\Repositories\ExternalBracketRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\GameGenerationRepository::class,
+            \App\Repositories\GameGenerationRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\GameRepository::class,
+            \App\Repositories\GameRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\ImageRepository::class,
+            \App\Repositories\ImageRepository::class);
+        $this->app->bind(
             \App\Contracts\Repositories\PersonRepository::class,
             \App\Repositories\PersonRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentFormatRepository::class,
+            \App\Repositories\TournamentFormatRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentPriceRepository::class,
+            \App\Repositories\TournamentPriceRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentPrizeRepository::class,
+            \App\Repositories\TournamentPrizeRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentRepository::class,
+            \App\Repositories\TournamentRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentRuleRepository::class,
+            \App\Repositories\TournamentRuleRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentSystemRepository::class,
+            \App\Repositories\TournamentSystemRepository::class);
+        $this->app->bind(
+            \App\Contracts\Repositories\TournamentTypeRepository::class,
+            \App\Repositories\TournamentTypeRepository::class);
         $this->app->bind(
             \App\Contracts\Repositories\UserRepository::class,
             \App\Repositories\UserRepository::class);
