27ea3c307a3d71db046a2802789cb53fed3008b3
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-01 15:54:52 -0500

    Added tournament service and test

---
+++ app/Services/Tournament/CreateCompleteTournamentService.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Services\Tournament;
+
+use App\Contracts\Repositories\ExternalBracketRepository;
+use App\Contracts\Repositories\FileRepository;
+use App\Contracts\Repositories\ImageRepository;
+use App\Contracts\Repositories\TournamentPriceRepository;
+use App\Contracts\Repositories\TournamentPrizeRepository;
+use App\Contracts\Repositories\TournamentRepository;
+use App\Models\ExternalBracket;
+use App\Models\Image;
+use App\Models\Tournament;
+use App\Models\TournamentPrice;
+use App\Models\TournamentPrize;
+use Illuminate\Support\Arr;
+
+class CreateCompleteTournamentService
+{
+    /**
+     * The tournament repository
+     *
+     * @var TournamentRepository
+     */
+    private TournamentRepository $tournamentRepository;
+
+    /**
+     * The external bracket repository
+     *
+     * @var ExternalBracketRepository
+     */
+    private ExternalBracketRepository $externalBracketRepository;
+
+    /**
+     * The tournament price repository
+     *
+     * @var TournamentPriceRepository
+     */
+    private TournamentPriceRepository $tournamentPriceRepository;
+
+    /**
+     * The tournament prize repository
+     *
+     * @var TournamentPrizeRepository
+     */
+    private TournamentPrizeRepository $tournamentPrizeRepository;
+
+    /**
+     * The tournament image repository
+     *
+     * @var ImageRepository
+     */
+    private ImageRepository $imageRepository;
+
+    /**
+     * The file repository
+     *
+     * @var FileRepository
+     */
+    private FileRepository $fileRepository;
+
+    /**
+     * Create new CreateCompleteTournamentService
+     *
+     * @param   TournamentRepository $tournamentRepository
+     * @param   ExternalBracketRepository $externalBracketRepository
+     * @param   TournamentPriceRepository $tournamentPriceRepository
+     * @param   TournamentPrizeRepository $tournamentPrizeRepository
+     * @param   ImageRepository $imageRepository
+     * @param   FileRepository  $fileRepository
+     * @return  void
+     */
+    public function __construct(
+        TournamentRepository $tournamentRepository,
+        ExternalBracketRepository $externalBracketRepository,
+        TournamentPriceRepository $tournamentPriceRepository,
+        TournamentPrizeRepository $tournamentPrizeRepository,
+        ImageRepository $imageRepository,
+        FileRepository $fileRepository
+    )
+    {
+        $this->tournamentRepository = $tournamentRepository;
+        $this->externalBracketRepository = $externalBracketRepository;
+        $this->tournamentPriceRepository = $tournamentPriceRepository;
+        $this->tournamentPrizeRepository = $tournamentPrizeRepository;
+        $this->imageRepository = $imageRepository;
+        $this->fileRepository = $fileRepository;
+    }
+
+    /**
+     * Creates a new tournament
+     *
+     * @param   array $data
+     * @return  Tournament
+     */
+    public function __invoke(array $data)
+    {
+        $image = $this->makeImage($data);
+
+        $tournament = $this->makeTournament($data, $image);
+
+        $this->makeAndAttachTournamentPrice($data, $tournament);
+
+        $this->makeAndAttachTournamentPrizes($data, $tournament);
+
+        $this->makeAndAttachExternalBracket($data, $tournament);
+
+        $tournament->games()->attach(Arr::pluck(Arr::get($data, 'games'), 'id'));
+        $tournament->devices()->attach(Arr::pluck(Arr::get($data, 'devices'), 'id'));
+        $tournament->tournamentSystems()->attach(Arr::pluck(Arr::get($data, 'tournament_systems'), 'id'));
+        $tournament->tournamentRules()->attach(Arr::pluck(Arr::get($data, 'tournament_rules'), 'id'));
+
+        return $tournament;
+    }
+
+    /**
+     * Make the base of the tournament
+     *
+     * @param   array $data
+     * @param   ?Image $image
+     * @return  Tournament
+     */
+    private function makeTournament(array $data, ?Image $image)
+    {
+        $tournament = new Tournament();
+
+        $tournament->title = $data['title'];
+        $tournament->description = $data['description'];
+        $tournament->place = $data['place'];
+        $tournament->start_date = $data['start_date'];
+        $tournament->end_date = $data['end_date'];
+        $tournament->created_by_person_id = Arr::get($data, 'created_by_person.id');
+        $tournament->tournament_type_id = Arr::get($data, 'tournament_type.id');
+        $tournament->tournament_format_id = Arr::get($data, 'tournament_format.id');
+
+        if (!is_null($image))
+        {
+            $tournament->image_id = $image->id;
+        }
+
+        $this->tournamentRepository->save($tournament);
+
+        return $tournament;
+    }
+
+    /**
+     * Make tournament price and attach to tournament
+     *
+     * @param   array $data
+     * @param   Tournament $tournament
+     * @return  void
+     */
+    private function makeAndAttachTournamentPrice(array $data, Tournament $tournament)
+    {
+        $tournamentPrice = new TournamentPrice();
+
+        $tournamentPrice->symbol = Arr::get($data, 'tournament_price.symbol');
+        $tournamentPrice->amount = Arr::get($data, 'tournament_price.amount');
+        $tournamentPrice->tournament_id = $tournament->id;
+
+        $this->tournamentPriceRepository->save($tournamentPrice);
+    }
+
+    /**
+     * Make tournament prizes and attach to tournament
+     *
+     * @param   array $data
+     * @param   Tournament $tournament
+     * @return  void
+     */
+    private function makeAndAttachTournamentPrizes(array $data, Tournament $tournament)
+    {
+        foreach ($data['tournament_prizes'] as $prizeData)
+        {
+            $tournamentPrize = new TournamentPrize();
+
+            $tournamentPrize->title = $prizeData['title'];
+            $tournamentPrize->description = $prizeData['description'];
+            $tournamentPrize->tournament_id = $tournament->id;
+
+            $this->tournamentPrizeRepository->save($tournamentPrize);
+        }
+    }
+
+    /**
+     * Make external bracket if exists and attach to tournament
+     *
+     * @param   array $data
+     * @param   Tournament $tournament
+     * @return  void
+     */
+    private function makeAndAttachExternalBracket(array $data, Tournament $tournament)
+    {
+        if (
+            !Arr::has($data, 'external_bracket') ||
+            (Arr::has($data, 'external_bracket') &&
+            is_null(Arr::get($data, 'external_bracket'))))
+        {
+            return;
+        }
+
+        $externalBracket = new ExternalBracket();
+
+        $externalBracket->reference = Arr::get($data, 'external_bracket.reference');
+        $externalBracket->url = Arr::get($data, 'external_bracket.url');
+        $externalBracket->tournament_id = $tournament->id;
+
+        $this->externalBracketRepository->save($externalBracket);
+
+        $tournament->externalBracket()->associate($externalBracket);
+    }
+
+    /**
+     * Make an image and file if exists
+     *
+     * @param   array $data
+     * @return  Image|null
+     */
+    private function makeImage(array $data)
+    {
+        if (!Arr::has($data, 'tournament_image_file') ||
+            (Arr::has($data, 'tournament_image_file') &&
+            is_null(Arr::get($data, 'tournament_image_file'))))
+        {
+            return null;
+        }
+
+        $imageFile = $data['tournament_image_file'];
+
+        $image = new Image();
+
+        $image->name = $imageFile->getClientOriginalName();
+        $image->url = '_';
+
+        $this->imageRepository->save($image);
+
+        $fileId = $this->fileRepository->save($imageFile, $image->id);
+
+        $image->url = $this->fileRepository->findUrl($fileId);
+
+        $this->imageRepository->save($image);
+
+        return $image;
+    }
+}
---
+++ tests/Unit/Services/Tournament/CreateCompleteTournamentServiceTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Contracts\Repositories\FileRepository;
+use App\Models\Device;
+use App\Models\Game;
+use App\Models\GameGeneration;
+use App\Models\Person;
+use App\Models\TournamentFormat;
+use App\Models\TournamentRule;
+use App\Models\TournamentSystem;
+use App\Models\TournamentType;
+use App\Repositories\CompetitorRepository;
+use App\Repositories\ExternalBracketRepository;
+use App\Repositories\GoogleDriveFileRepository;
+use App\Repositories\ImageRepository;
+use App\Repositories\PersonRepository;
+use App\Repositories\TournamentFormatRepository;
+use App\Repositories\TournamentPriceRepository;
+use App\Repositories\TournamentPrizeRepository;
+use App\Repositories\TournamentRepository;
+use App\Repositories\TournamentTypeRepository;
+use App\Repositories\UserRepository;
+use App\Services\Tournament\CreateCompleteTournamentService;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
+use Tests\TestCase;
+
+class CreateCompleteTournamentServiceTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * @test
+     */
+    public function a_complete_tournament_should_be_created()
+    {
+
+        // FIXME: Needs to be mocked
+        $service = new CreateCompleteTournamentService(
+            new TournamentRepository(),
+            new ExternalBracketRepository(),
+            new TournamentPriceRepository(),
+            new TournamentPrizeRepository(),
+            new ImageRepository(),
+            new GoogleDriveFileRepository()
+        );
+
+        $person = Person::factory()->create();
+        $tournamentType = TournamentType::factory()->create();
+        $tournamentFormat = TournamentFormat::factory()->create();
+        $generation = GameGeneration::factory()->create([
+            'generation' => '1',
+        ]);
+        $games = Game::factory()->count(2)->create([
+            'game_generation_id' => $generation->id,
+        ]);
+        $devices = Device::factory()->count(3)->create();
+        $tournamentSystems = TournamentSystem::factory()->count(1)->create();
+        $tournamentRules = TournamentRule::factory()->count(6)->create();
+
+        $data = [
+            'title' => 'Some tournament',
+            'description' => 'some description',
+            'place' => 'Some place',
+            'start_date' => '2022-01-01 08:08:00',
+            'end_date' => '2022-02-02 08:08:00',
+            'created_by_person' => [
+                'id' => $person->id,
+            ],
+            'tournament_type' => [
+                'id' => $tournamentType->id,
+            ],
+            'tournament_format' => [
+                'id' => $tournamentFormat->id,
+            ],
+            'tournament_price' => [
+                'symbol' => 'S/.',
+                'amount' => 5.0,
+            ],
+            'tournament_prizes' => [
+                [
+                    'title' => 'Amiibo Samus',
+                    'description' => 'Un amibo de Samus',
+                ],
+                [
+                    'title' => '10 Soles',
+                    'description' => '10 soles vía yape',
+                ],
+            ],
+            'external_bracket' => [
+                'reference' => 'challonge',
+                'url' => 'https://test.com.pe/1234'
+            ],
+            'tournament_image_file' => null,
+            'games' => $games->toArray(),
+            'devices' => $devices->toArray(),
+            'tournament_systems' => $tournamentSystems->toArray(),
+            'tournament_rules' => $tournamentRules->toArray(),
+        ];
+
+        $tournament = call_user_func($service, $data);
+
+        $this->assertEquals(Arr::get($data, 'title'), $tournament->title);
+        $this->assertEquals(Arr::get($data, 'description'), $tournament->description);
+        $this->assertEquals(Arr::get($data, 'place'), $tournament->place);
+        $this->assertEquals(Arr::get($data, 'start_date'), $tournament->start_date);
+        $this->assertEquals(Arr::get($data, 'end_date'), $tournament->end_date);
+        $this->assertEquals(Arr::get($data, 'created_by_person.id'), $tournament->created_by_person_id);
+        $this->assertEquals(Arr::get($data, 'tournament_type.id'), $tournament->tournament_type_id);
+        $this->assertEquals(Arr::get($data, 'tournament_format.id'), $tournament->tournament_format_id);
+        $this->assertEquals(Arr::get($data, 'tournament_price.symbol'), $tournament->tournamentPrice->symbol);
+        $this->assertEquals(Arr::get($data, 'tournament_price.amount'), $tournament->tournamentPrice->amount);
+        $this->assertCount(count(Arr::get($data, 'tournament_prizes')), $tournament->tournamentPrizes);
+        $this->assertEquals(Arr::get($data, 'external_bracket.reference'), $tournament->externalBracket->reference);
+        $this->assertEquals(Arr::get($data, 'external_bracket.url'), $tournament->externalBracket->url);
+        $this->assertCount(count(Arr::get($data, 'games')), $tournament->games);
+        $this->assertCount(count(Arr::get($data, 'devices')), $tournament->devices);
+        $this->assertCount(count(Arr::get($data, 'tournament_systems')), $tournament->tournamentSystems);
+        $this->assertCount(count(Arr::get($data, 'tournament_rules')), $tournament->tournamentRules);
+        $this->assertEmpty($tournament->image);
+    }
+}
