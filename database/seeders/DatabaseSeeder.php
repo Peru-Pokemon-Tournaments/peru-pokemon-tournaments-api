6bf747f27a9b3ad0550467e5d9019bec2e876d7d
Jibaru <ignacioruedaboada@hotmail.com>
2022-03-31 22:48:56 -0500

    Uncommented user person competitor seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            //UserPersonCompetitorSeeder::class,
+            UserPersonCompetitorSeeder::class,
             BaseDevicesSeeder::class,
             BaseGameGenerationsAndGamesSeeder::class,
             BaseTournamentFormatsSeeder::class,
