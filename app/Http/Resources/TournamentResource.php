9cfe5f24369b488bd9a54d5a59b9b2d4133e89f1
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-01 19:38:31 -0500

    Added tournament resource

---
+++ app/Http/Resources/TournamentResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TournamentResource extends JsonResource
+{
+    /**
+     * The resource instance.
+     *
+     * @var \App\Models\Tournament
+     */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return array_merge(
+            $this->resource->toArray(),
+            [
+                'image' => $this->resource->image,
+                'tournament_type' => $this->resource->tournamentType,
+                'devices' => $this->resource->devices,
+                'games' => $this->resource->games,
+                'tournament_format' => $this->resource->tournamentFormat,
+                'tournament_price' => $this->resource->tournamentPrice,
+                'tournament_systems' => $this->resource->tournamentSystems,
+            ],
+        );
+    }
+}
