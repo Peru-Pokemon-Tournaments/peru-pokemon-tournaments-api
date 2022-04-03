fca25d87d898d1749e73e381dd5b9ef2be673a1f
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 14:27:45 -0500

    Added method to find competitors in tournament

--- app/Contracts/Repositories/CompetitorRepository.php
+++ app/Contracts/Repositories/CompetitorRepository.php
@@ -4,5 +4,10 @@ namespace App\Contracts\Repositories;
 
 interface CompetitorRepository extends Repository
 {
-
+    /**
+     * Retrieves all competitors enrolled in that tournament
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function findManyEnrolledToTournament(string $tournamentId);
 }
--- app/Repositories/CompetitorRepository.php
+++ app/Repositories/CompetitorRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Contracts\Repositories\CompetitorRepository as CompetitorRepositoryContract;
 use App\Models\Competitor;
+use App\Models\TournamentInscription;
 use App\Traits\Repositories\CommonMethods;
 
 final class CompetitorRepository implements CompetitorRepositoryContract
@@ -39,5 +40,21 @@ final class CompetitorRepository implements CompetitorRepositoryContract
     public function findMany(array $ids)
     {
         return Competitor::findMany($ids);
+    }
+
+    /**
+     * Retrieves all competitors enrolled in that tournament
+     *
+     * @param  string $tournamentId
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function findManyEnrolledToTournament(string $tournamentId)
+    {
+        return TournamentInscription::where('tournament_id', $tournamentId)
+            ->get()
+            ->map(function (TournamentInscription $tournamentInscription) {
+                return $tournamentInscription->competitor;
+            })
+            ->collect();
     }
 }
