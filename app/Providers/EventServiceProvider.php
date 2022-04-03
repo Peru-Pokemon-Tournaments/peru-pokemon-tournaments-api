f216eae7d41c50afdb27b2816b834acc5ddaf0f9
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 13:35:49 -0500

    Added new tournament inscription created event

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \App\Events\TournamentInscriptionCreated::class => [
+            \App\Listeners\SendTournamentCreatedEmailNotification::class,
+        ],
     ];
 
     /**
