85b6e3b5192bc1429aabd1b51844e93838645ae3
Jibaru <ignacioruedaboada@hotmail.com>
2022-04-03 15:57:29 -0500

    Created update tournament inscription status request

---
+++ app/Http/Requests/UpdateTournamentInscriptionStatusRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\TournamentInscription;
+use App\Traits\Requests\FailedValidationResponse;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateTournamentInscriptionStatusRequest extends FormRequest
+{
+    use FailedValidationResponse;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'status' => ['required', 'string', Rule::in([
+                TournamentInscription::ACCEPTED,
+                TournamentInscription::PENDING,
+                TournamentInscription::REJECTED,
+            ])],
+        ];
+    }
+}
