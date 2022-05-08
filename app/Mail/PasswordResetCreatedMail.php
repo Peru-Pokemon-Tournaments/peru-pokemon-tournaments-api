f1c91c43ef896688b690dd0594853df710ccb7bd
Jibaru <ignacioruedaboada@hotmail.com>
2022-05-07 20:18:38 -0500

    Added event, listeners and mail to send when password reset it is created or updated

---
+++ app/Mail/PasswordResetCreatedMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetCreatedMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * The password reset url
+     *
+     * @var string
+     */
+    public string $resetUrl;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param  string $resetUrl
+     * @return void
+     */
+    public function __construct(
+        string $resetUrl
+    )
+    {
+        $this->resetUrl = $resetUrl;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.password-reset-created')
+                    ->subject('Enlace para cambio de contraseña');
+    }
+}
