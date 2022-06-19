a09bfb0884cf2ab9844f907a959b1a850f08a14a
Jibaru <ignacioruedaboada@hotmail.com>
2022-06-19 18:34:35 -0500

    Fixed message on fetch game generations controller

--- app/Http/Controllers/GameGeneration/FetchGameGenerationsController.php
+++ app/Http/Controllers/GameGeneration/FetchGameGenerationsController.php
@@ -46,7 +46,7 @@ class FetchGameGenerationsController extends PaginatedController
         );
 
         return $this->paginatedResponseBuilder
-            ->setMessage(trans('endpoints.game_generations.fetch_game_generations.ok'))
+            ->setMessage(trans('endpoints.game_generation.fetch_game_generations.ok'))
             ->setResources('game_generations', GameGenerationResource::collection($gameGenerationsPaginated))
             ->setLengthAwarePaginator($gameGenerationsPaginated)
             ->setStatusCode(Response::HTTP_OK)
